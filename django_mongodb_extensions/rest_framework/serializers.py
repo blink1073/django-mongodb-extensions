6bc3a64041ad9390a09b268517cc6539aca64c7e
Steven Silvester <steve.silvester@mongodb.com>
2026-06-15 20:07:54 -0500

    Update django_mongodb_extensions/rest_framework/serializers.py
    Co-authored-by: Tim Graham <timograham@gmail.com>

--- django_mongodb_extensions/rest_framework/serializers.py
+++ django_mongodb_extensions/rest_framework/serializers.py
@@ -218,7 +218,7 @@ class EmbeddedModelSerializer(serializers.Serializer):
         ), f"Class {self.__class__.__name__}.Meta missing 'fields' attribute."
 
         embedded_model: type[Any] = meta.model
-        all_fields = {f.name: f for f in embedded_model._meta.fields}
+        all_fields_names = {f.name: f for f in embedded_model._meta.fields}
 
         explicit_fields = meta.fields != ALL_FIELDS
         field_names: list[str] | str = meta.fields
